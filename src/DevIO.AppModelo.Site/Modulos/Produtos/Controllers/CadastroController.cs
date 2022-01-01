1123b284125e987496f141d9e80c49db4164bc40
Alessandro Oliveira <alessandro.ibes@gmail.com>
2021-12-31 22:39:25 -0300

    Adicionado Areas: Produtos e Vendas

---
+++ src/DevIO.AppModelo.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.AppModelo.Site.Modulos.Produtos.Controllers
+{
+    [Area("Produtos")]
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Busca()
+        {
+            return View();
+        }
+    }
+}
--- src/DevIO.AppModelo.Site/Startup.cs
+++ src/DevIO.AppModelo.Site/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DevIO.AppModelo.Site
@@ -11,6 +12,14 @@ namespace DevIO.AppModelo.Site
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RazorViewEngineOptions>(options => 
+            {
+                options.AreaViewLocationFormats.Clear();
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+                options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -27,6 +36,11 @@ namespace DevIO.AppModelo.Site
             app.UseMvc(routes => 
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+                //routes.MapRoute("areas", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapAreaRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+                routes.MapAreaRoute("AreaVendas", "Vendas", "Vendas/{controller=Pedidos}/{action=Index}/{id?}");
             });
         }
     }
