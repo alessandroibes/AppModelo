10a69ce87edd79ad4f06eb8d3f7c5576553b2bac
Alessandro Oliveira <alessandro.ibes@gmail.com>
2022-01-01 15:39:07 -0300

    Adicionado Migrations

---
+++ src/DevIO.AppModelo.Site/Controllers/TesteCrudController.cs
@@ -0,0 +1,44 @@
+﻿using DevIO.AppModelo.Site.Data;
+using DevIO.AppModelo.Site.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace DevIO.AppModelo.Site.Controllers
+{
+    public class TesteCrudController : Controller
+    {
+        private readonly MeuDbContext _contexto;
+
+        public TesteCrudController(MeuDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno
+            {
+                Nome = "Alessandro",
+                DataNascimento = DateTime.Now.AddYears(-40),
+                Email = "alessandro@gmail.com"
+            };
+
+            _contexto.Alunos.Add(aluno);
+            _contexto.SaveChanges();
+
+            var aluno2 = _contexto.Alunos.Find(aluno.Id);
+            var aluno3 = _contexto.Alunos.FirstOrDefault(a => a.Email == "alessandro@gmail.com");
+            var aluno4 = _contexto.Alunos.Where(a => a.Nome == "Alessandro");
+
+            aluno.Nome = "João";
+            _contexto.Alunos.Update(aluno);
+            _contexto.SaveChanges();
+
+            _contexto.Alunos.Remove(aluno);
+            _contexto.SaveChanges();
+
+            return View();
+        }
+    }
+}
--- src/DevIO.AppModelo.Site/Startup.cs
+++ src/DevIO.AppModelo.Site/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
@@ -13,6 +15,13 @@ namespace DevIO.AppModelo.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<RazorViewEngineOptions>(options => 
@@ -22,6 +31,10 @@ namespace DevIO.AppModelo.Site
                 options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
                 options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
             });
+
+            services.AddDbContext<MeuDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("MeuDbContext"))
+            );
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
